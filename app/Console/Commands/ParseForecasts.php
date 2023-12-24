69acb42acdffa1287341ac55927beffdde36b9b1
rozhkov <po6uh86@gmail.com>
2023-12-24 10:03:02 -0500

    added libs and command for parsing

---
+++ app/Console/Commands/ParseForecasts.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ParseForecasts extends Command
+{
+    protected $signature = 'parse:forecasts';
+
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        //
+    }
+}
