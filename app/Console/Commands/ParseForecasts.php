81ce8508ec7dc831f8a1f3db8c88ded12b45b04b
rozhkov <po6uh86@gmail.com>
2023-12-28 03:45:15 -0500

    added filter for sport type

--- app/Console/Commands/ParseForecasts.php
+++ app/Console/Commands/ParseForecasts.php
@@ -51,7 +51,7 @@ class ParseForecasts extends Command
             $sportType = $node->filter('.forecast-preview__league')->first()->text();
             $typesForExclude = [
                 'Волейбол', 'КХЛ', 'Настольный теннис', 'Желтые карточки', 'Лига Алеф', 'Чемпионат ОАЭ', 'Егип', 'Гана',
-                'Бахрейн', 'Kontinental Hockey League', 'Чемпионат Бельгии. Премьер-лига'
+                'Бахрейн', 'Kontinental Hockey League', 'Чемпионат Бельгии. Премьер-лига', 'Киберспорт'
             ];
 
             foreach ($typesForExclude as $value) {
