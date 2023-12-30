c97d5dd5ae8da90139f8339103bc24c225611240
rozhkov <po6uh86@gmail.com>
2023-12-30 15:30:08 -0500

    decreased time between parses

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,7 +9,7 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command('parse:forecasts')->everyTenMinutes();
+         $schedule->command('parse:forecasts')->everyFiveMinutes();
     }
 
     protected function commands(): void
