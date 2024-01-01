3f4d7833cdd87d4f37e4920bc20ad72173c3a67f
rozhkov <po6uh86@gmail.com>
2024-01-01 11:30:25 -0500

    changed needed profit

--- app/Console/Commands/ParseForecasts.php
+++ app/Console/Commands/ParseForecasts.php
@@ -224,7 +224,7 @@ class ParseForecasts extends Command
             ->text();
         $profit = (int)round((float)$node->filter('.forecast-preview__author-stat-item span')->last()->text());
 
-        if ($coefficient < 1.3 || $profit < 28) {
+        if ($coefficient < 1.3 || $profit < 0) {
             return null;
         }
 
