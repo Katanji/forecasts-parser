6925745a87a161d94984e694a0fc6579c6cf3dc7
rozhkov <po6uh86@gmail.com>
2024-01-28 07:16:23 -0500

    increased profit

--- app/Console/Commands/ParseForecasts.php
+++ app/Console/Commands/ParseForecasts.php
@@ -126,7 +126,7 @@ class ParseForecasts extends Command
             }
 
             $profit = (int)round((float)$node->filter('.forecast-preview__author-stat-item span')->last()->text());
-            if ($profit < 56) {
+            if ($profit < 62) {
                 return;
             }
 
@@ -220,7 +220,7 @@ class ParseForecasts extends Command
             ->text();
         $profit = (int)round((float)$node->filter('.forecast-preview__author-stat-item span')->last()->text());
 
-        if ($coefficient < 1.3 || $profit < 56) {
+        if ($coefficient < 1.3 || $profit < 62) {
             return null;
         }
 
